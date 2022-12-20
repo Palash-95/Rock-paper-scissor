e41317dc620633b92b63bead8084a128a69f07b5
Palash.95 <palashmalakar103@gmail.com>
2022-12-20 18:28:31 +0530

    write the basic algorithm for the program

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// write a function to get computer choice
+//write a function called playRound with two parameters computerSelection and userSelection to play the game 
+//write a function called game() to play the game 5 times
+//call getComputerChoice function and store the returned value in a variable
+//prompt the user for a input and store it in a variable ,make it case insensitive
+//call the game() function
