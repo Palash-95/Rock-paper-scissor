fcbb4d05d41ff387e86c87b70fcee93b267423aa
Palash.95 <palashmalakar103@gmail.com>
2022-12-20 19:22:34 +0530

    creat two variable and assign values to them
    computerSelection and userSelection

--- javascript.js
+++ javascript.js
@@ -11,5 +11,9 @@ console.log(getComputerChoice())
 //write a function called playRound with two parameters computerSelection and userSelection to play the game 
 //write a function called game() to play the game 5 times
 //call getComputerChoice function and store the returned value in a variable
+let computerSelection = getComputerChoice();
+console.log(computerSelection)
 //prompt the user for a input and store it in a variable ,make it case insensitive
+let userSelection = prompt("Enter your choice :","").toLowerCase();
+console.log(userSelection)
 //call the game() function
